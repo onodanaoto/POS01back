4f924eacf7695449dddef91254fe6828cb257e23
onodanaoto <onoda.rakudasystem@gmail.com>
2025-02-15 18:55:23 +0900

    feat: Add SSL configuration for Azure MySQL connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,13 +3,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Azure MySQL用の接続文字列
+# Azure MySQL用の接続文字列（SSL設定を追加）
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "mysql://Tech0Gen8TA4@tech0-gen-8-step4-db-4.mysql.database.azure.com:3306/pos_db"
+    "mysql+pymysql://Tech0Gen8TA4@tech0-gen-8-step4-db-4.mysql.database.azure.com:3306/pos_db?ssl=true"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/certs/ca-certificates.crt"
+        }
+    }
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
