14d5e8c24c27d219044a7ea7f162e550cdeac21c
onodanaoto <onoda.rakudasystem@gmail.com>
2025-02-15 18:22:33 +0900

    fix: Update database connection string for Azure MySQL

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+# Azure MySQL用の接続文字列
 DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "mysql://user:sakepara2025@tech0-gen8-step4-pos-db.mysql.database.azure.com:3306/pos_db"
+    "DATABASE_URL",
+    "mysql://Tech0Gen8TA4@tech0-gen-8-step4-db-4.mysql.database.azure.com:3306/pos_db"
 )
 
 engine = create_engine(DATABASE_URL)
